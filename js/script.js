8c95eb0ffb0c8da64ab9003491bc4b2b62e2b1db
Md. Wasek Bellah <wasekbellah2311@gmail.com>
2021-07-11 10:54:21 +0600

    inspiration section

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ document.addEventListener('scroll', function () {
     window.scrollY >= 20 ? sticky.classList.add('stickyAdd') : sticky.classList.remove('stickyAdd');
 });
 
+// Hero Slider
 new Swiper('.heroSlider', {
     // Optional parameters
     direction: 'horizontal',
@@ -25,13 +26,46 @@ new Swiper('.heroSlider', {
 
     // If we need pagination
     pagination: {
-        el: '.swiper-pagination',
+        el: '.heroSlider .swiper-pagination',
         clickable: true
     },
 
     // Navigation arrows
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.heroSlider .swiper-button-next',
+        prevEl: '.heroSlider .swiper-button-prev',
+    }
+});
+
+// Inspections Slider
+new Swiper('.inspirationSlider', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 2,
+    spaceBetween: 24,
+    loop: true,
+    autoplay: {
+        delay: 1000,
+    },
+    speed: 1000,
+    effect: '',
+    fadeEffect: {
+        crossFade: true
+    },
+    allowTouchMove: false,
+    allowSlideNext: true,
+    allowSlidePrev: true,
+    autoHeight: false,
+    notificationClass: null,
+
+    // If we need pagination
+    pagination: {
+        el: '.inspirationSlider .swiper-pagination',
+        clickable: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.inspirationSlider .swiper-button-next',
     }
 });
